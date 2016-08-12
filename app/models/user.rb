06cfc68104503cb4fe37197273bdeb1ccb743296
Lúcio Charallo <luciotbc@gmai.com>
2016-08-12 09:47:49 -0300

    Devise in pt-BR

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :confirmable, :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
